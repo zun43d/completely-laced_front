aae4f48d4f5453485755e893c54fa29fda1f1427
Zunaed Ahmed <zunaedahmed.info@gmail.com>
2021-11-29 23:41:19 +0600

    UI part done

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,53 @@
+import { useRef, useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+	const emailEl = useRef();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const email = emailEl.current.value;
+	};
+
 	return (
 		<div className={styles.container}>
-			<h1>Currently under construstion</h1>
+			<Head>
+				<title>Completely Laced | Opening soon</title>
+			</Head>
+
+			<div className={styles.logo}>
+				<Image
+					src="/completely-laced.png"
+					width={300}
+					height={300}
+					alt="Completely Laced"
+					layout="responsive"
+				/>
+			</div>
+			<h1 className={styles.open}>Opening Soon</h1>
+			<p className={styles.first}>Be the first to know when we launch</p>
+			<div className={styles.promo} style={{ textAlign: 'center' }}>
+				Promotions, new offers and sales. Directly to your inbox.
+			</div>
+			<form
+				onSubmit={handleSubmit}
+				className={styles.form}
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<input
+					type="email"
+					placeholder="Enter your email address"
+					id=""
+					ref={emailEl}
+				/>
+				<button type="submit">Sign up</button>
+			</form>
 		</div>
 	);
 }
